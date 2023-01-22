255e6ed97c1705921ef6014917719b56611098b6
tommys7 <tomas.hojgr23@gmail.com>
2023-01-22 13:08:10 +0100

    Kodovani slideru a footeru, responzivita, animace a validace formulare

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
+    new WOW().init();
+
+
     var button = $('#mainButton');
+
+
 
     button.on('mouseenter', function(){
         button.addClass('animate__headShake')
@@ -9,5 +14,66 @@ $(document).ready(function(){
         button.removeClass('animate__headShake')
     });
     
+
     
+
+
+    
+
+    $('.portfolio-slider').slick({
+        dots: true,
+        slidesToShow : 1,
+        arrows : false,
+        
+    });
+    
+
+
+    $('#brief-form').validate({
+      rules: {
+        username: "required",
+        telephone: "required"
+      },
+      messages: {
+        username: "Vyplňte prosím vaše jméno",
+        telephone: "Vyplňte prosím vaše tel. číslo"
+      }
+    });
+
+
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 });
